cc21859c8d8c1687cc1c6366b37a6b4a09995bdf
David Ferreira de Almeida <ferreiradavid361@gmail.com>
2022-04-04 08:55:15 -0300

    Add files via upload

---
+++ Configurar PHP no VSCode/index.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+class Main
+{
+    private string $name;
+
+    public function __construct()
+    {
+        echo "Hello\n";
+    }
+
+    /**
+     * Just a test
+     *
+     * @param string $name
+     * @return void
+     */
+    public function teste(string $name): void
+    {
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
